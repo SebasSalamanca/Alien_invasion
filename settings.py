34d04423939d0d98e19fffefdf19d0e30dbee934
sebas_salamanca24 <jsebastian1119@gmail.com>
2025-09-11 22:32:46 -0500

    initialize basic configurations and display screen

---
+++ settings.py
@@ -0,0 +1,12 @@
+
+
+class Settings:
+    """A class to store all settings for alien invasion"""
+    
+    def __init__(self):
+        """Initialize game settings"""
+        #Screen settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (0,0,0)
+        
