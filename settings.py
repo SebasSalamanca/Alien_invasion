379b859356e7d0b994eb614125dfaf8ee39e688e
sebas_salamanca24 <jsebastian1119@gmail.com>
2025-09-16 12:33:11 -0500

    add feature limit the movement of the aliens at the edges of the screen

--- settings.py
+++ settings.py
@@ -8,15 +8,22 @@ class Settings:
         #Screen settings
         self.screen_width = 1200
         self.screen_height = 800
-        self.bg_color = (255,255,0)
+        self.bg_color = (125,125,125)
 
         #Ship settings
-        self.ship_speed = 3.5
+        self.ship_speed = 5
 
         #Bullet Settings
-        self.bullet_speed = 5.0
+        self.bullet_speed = 7.0
         self.bullet_width = 4
         self.bullet_height = 15
         self.bullet_color = (255, 165, 0)
         self.bullets_allowed = 7
+
+        #Alien settings
+        self.alien_speed = 2.5
+        self.fleet_drop_speed = 10
+        #Fleet direction of 1 represents right; -1 represents left
+        self.fleet_direction = 1
+
         
